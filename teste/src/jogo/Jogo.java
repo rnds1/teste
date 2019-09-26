f91e990453a921f6e3bea130130abd8e35e2743f
Rafael <Rafael@DESKTOP-0P4ENMS>
2019-09-26 11:33:42 -0300

    remake with base

---
+++ teste/src/jogo/Jogo.java
@@ -0,0 +1,110 @@
+package jogo;
+
+
+public class Jogo{
+    
+      /*  
+	private String palavra;
+	private String mascara;
+	public int chances = 6;
+	private int w; */
+	public static String ex[]	 = {
+	"|||||||", //1
+	"|   o", //2 
+	"|  /|\\", //3
+	"| / | \\", //4
+	"|   |", //5
+	"|  / \\", //6
+	"| /   \\\n\n YOU LOSE OTARO."}; //7
+	//funcionando
+	/*
+	public void jogar(){
+		this.palavra = "abcdef";
+		//funcionando
+                
+		//System.out.println("AFB:"+ this.palavra);
+		
+                
+	}
+	*/
+	
+	/*
+	//define a mascara
+	public void setMascara(){
+            
+            //Funcionando
+		//this.mascara = this.palavra;
+		int i = this.palavra.length();
+                char[] b = this.palavra.toCharArray();
+                for(int x =0;x<i;x++){
+                //HERE chance all characters 
+                b[x] = '-';
+                    
+                }
+                this.mascara = String.valueOf(b);
+		System.out.println(mascara);
+	}
+        */
+	/*public void mostrarMascara(){
+            System.out.println(mascara);
+            
+            //done
+		//System.out.println("mostrando mascara");
+	}*/
+	
+	public static void mostrarForca(int x){
+            //Funcionando
+	
+		for(int w =0;w < x;w ++){ 
+	System.out.println(Jogo.ex[w]);
+		}
+	
+	
+	}
+	/*
+	
+	public void jogadaN(char[] x){
+            boolean contem = false;
+                if(this.chances <= 0){
+                    mostrarForca(7-this.chances);
+                }else{
+            
+                //Funcionando
+                
+		char[] b = this.palavra.toCharArray();
+                int i,max;
+                max = this.palavra.length();
+              for(i=0;i<max;i++){
+		if(b[i] == x[0]  ){
+                    contem = true;
+                    subistituir(i, x);
+                    //System.out.println("xfx");
+                    
+                    } else {contem =false;}
+              }//end for
+                
+                System.out.println(this.mascara);
+                
+                
+              } // if
+		if(contem == false){
+                this.chances --;
+                }
+	}//end void
+	
+	public void subistituir(int pox, char c){
+            
+        
+        }
+
+    private void subistituir(int i, char[] x) {
+        char[] d = this.mascara.toCharArray();
+            d[i] = x[0];
+            this.mascara = String.valueOf(d);
+            
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+	
+	*/
+	
+}
---
+++ teste/src/jogo/Main.java
@@ -0,0 +1,14 @@
+package jogo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Rafael
+ */
+public class Main {
+    
+}
